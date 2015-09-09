49ca94cb0c305920451ded7dfcab2b115f76dfd8
Miguel Peixe <miguel@cardume.art.br>
2015-09-09 18:31:50 -0300

    use older version for easier config

--- app.js
+++ app.js
@@ -7,17 +7,26 @@ var Windshaft = require('windshaft');
 var _         = require('underscore');
 
 // Force 'development' environment
-var ENV = 'development';
+// var ENV = 'development';
 var PORT = 4000;
 
 // set environment specific variables
-global.settings     = require('windshaft/config/settings');
-global.environment  = require('./config');
-_.extend(global.settings, global.environment);
+global.environment = {
+  postgres: {
+    user: 'postgres',
+    host: '127.0.0.1',
+    port: 5432,
+    srid: 4326
+  },
+  millstone: {
+    cache_basedir: '/tmp/windshaft/millstone'
+  },
+  oneDay: 86400000
+};
 
 var config = {
   base_url: '/database/:dbname/table/:table',
-  base_url_mapconfig: '/tiles',
+  base_url_notable: '/database/:dbname',
   grainstore: {
     datasource: {
       user: 'postgres',
@@ -30,48 +39,23 @@ var config = {
   renderCache: {
     ttl: 60000 // seconds
   },
+  enable_cors: true,
   mapnik: {
     metatile: 4,
     bufferSize: 64
   },
-  redis: {host: '127.0.0.1', port: 6379},
-  // enable_cors: true,
+  redis: {
+    host: '127.0.0.1',
+    port: 6379
+  },
   req2params: function(req, callback){
-
-    req.params.dbname = 'mapaguarani';
-
-    // no default interactivity. to enable specify the database column you'd like to interact with
-    req.params.interactivity = null;
-
-    // this is in case you want to test sql parameters eg ...png?sql=select * from my_table limit 10
-    req.params =  _.extend({}, req.params);
+    req.params = _.extend({}, req.params);
     _.extend(req.params, req.query);
-
-    req.params.config = {
-      srid: 4326,
-      layers: [
-        {
-          type: 'mapnik',
-          options: {
-            sql: 'SELECT * FROM core_indigenousland',
-            cartocss: req.query.styles || '#core_indigenousland { polygon-fill: #000; }',
-            cartocss_version: '2.0.1',
-            geom_column: 'polygon',
-            geom_type: 'geometry',
-            srid: 4326
-          }
-        }
-      ]
-    };
-    req.params.config = JSON.stringify(req.params.config);
-
-    // send the finished req object on
-    callback(null,req);
+    callback(null,req)
   }
 };
 
 // Initialize tile server
 var ws = new Windshaft.Server(config);
 ws.listen(PORT);
-
-console.log("map tiles are now being served out of: http://localhost:" + PORT + config.base_url_mapconfig);
+console.log("map tiles are now being served out of: http://localhost:4000" + config.base_url + '/:z/:x/:y.*');
